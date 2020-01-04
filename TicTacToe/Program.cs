0772b051d633c6e61de6afec9884d9093887c69e
johneckert <johnteckert@gmail.com>
2020-01-04 10:53:23 -0500

    Initial Commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOver = false;
+
+            string[,] board = new string[,]
+            {
+                { " ", " ", " " },
+                { " ", " ", " " },
+                { " ", " ", " " }
+            };
+
+            printBoard(board);
+
+            void isGameOver()
+            {
+
+            }
+
+            void printBoard(string[,] board)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("+---+---+---+");
+                    Console.Write("|");
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write(" {0} ", board[i, j]);
+                        Console.Write("|");
+                    }
+                    Console.Write("\n");
+                }
+                Console.WriteLine("+---+---+---+");
+
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
